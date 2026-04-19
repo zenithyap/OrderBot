0baf44e19fc57895786145c2cd88b72293ee8d8f
zenith <zenithyap88@gmail.com>
2026-04-19 17:43:39 +0800

    fix pricing

--- bot/index.js
+++ bot/index.js
@@ -49,11 +49,11 @@ const menu = {
 export const getPrice = (selections) => {
   const items = Object.entries(selections || {});
   const totalQuantity = items.reduce((total, [sticker, qty]) => total + qty, 0);
-  if (totalQuantity <= 2) {
+  if (totalQuantity >= 0 && totalQuantity <= 2) {
     return totalQuantity * 2;
-  } else if (totalQuantity <= 5) {
+  } else if (totalQuantity >= 3 && totalQuantity <= 4) {
     return totalQuantity * 1.8;
-  } else if (totalQuantity <= 8) {
+  } else if (totalQuantity >= 5 && totalQuantity <= 9) {
     return totalQuantity * 1.6;
   } else {
     return totalQuantity * 1.5;
